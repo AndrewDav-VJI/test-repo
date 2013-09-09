bdd229c4eb04107bafc044b9fab37c4bd78e8a11
visualjazz-ADavidson <ADavidson@visualjazz.com.au>
2013-09-09 16:51:30 +1000

    Instantiation of the Maven module

---
+++ app/src/main/java/Repo_Test/app/App.java
@@ -0,0 +1,13 @@
+package Repo_Test.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
